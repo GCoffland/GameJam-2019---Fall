bd2f5ba2e7382ab644ff4512e5beaaec8a9ae0d5
Gabriel Coffland <35353420+GCoffland@users.noreply.github.com>
2019-11-03 19:35:21 -0800

    s

--- Assets/RandomRespawners.cs
+++ Assets/RandomRespawners.cs
@@ -6,6 +6,7 @@ public class RandomRespawners : MonoBehaviour
 {
 
     GameObject respawner;
+    float cooldown;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +15,29 @@ public class RandomRespawners : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (cooldown < 0 && GameScript.gs.deadPlayers.Count > 0)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Vector3Int n = new Vector3Int(Random.Range(0, StageGrid.instance.worldStatusArray.GetLength(0)), Random.Range(0, StageGrid.instance.worldStatusArray.GetLength(1)), 0);
+
+                if (StageGrid.instance.worldStatusArray[n.x, n.y] == StageGrid.STATUS.UNOCCUPIED)
+                {
+                    Vector3 y = StageGrid.instance.GetWorldFromCell(n);
+                    y.x += StageGrid.instance.tilemaps[0].cellBounds.xMin + 0.5f;
+                    y.y += StageGrid.instance.tilemaps[0].cellBounds.yMin + 0.5f;
+                    Instantiate(respawner, y, Quaternion.identity);
+                    flag = false;
+                }
+            }
+            cooldown = 45;
+        }
+        if(GameScript.gs.deadPlayers.Count > 0)
+        {
+            cooldown -= Time.fixedDeltaTime;
+        }
     }
 }
